5c3151f365b5f37b2e6e3313b97601e692491376
Mahammed Haque <Mahammed_Haque@epam.com>
2017-07-12 13:13:45 +0530

    quick sortjs implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,20 @@
 'use strict';
 
 function quickSort(input) {
-
+    if(!input && !input.isArray())
+    {
+        return;
+    }
+    if(input && input.length==0){
+        return input;
+    } else if(input.length ==1){
+        return input;
+    }
+    if(input.every(function(element){return isNaN(element)})){
+        return input.sort();
+    }
+    else{
+        return input.sort(function(a,b){return a-b;});
+    }
 };
 module.exports = quickSort;
